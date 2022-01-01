c1254343f8c47f0f5cd5cc41c52feaf3feddc057
Selvarajan Thangavel <selvarajan.thangavel@d4t4solutions.com>
2022-01-01 14:44:57 +0530

    Initial Commit

---
+++ BabyLoo/BabyLooApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyLooApp.swift
+//  BabyLoo
+//
+//  Created by Selvarajan on 01/01/22.
+//
+
+import SwiftUI
+
+@main
+struct BabyLooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
